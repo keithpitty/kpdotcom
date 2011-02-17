5185af27f5b91d4cb8389766e7d3bf3ea57a8977
Keith Pitty <keith@keithpitty.com>
2011-02-17 23:53:04 +1100

    More Rails 3 compatibility code changes.

--- config/environment.rb
+++ config/environment.rb
@@ -3,23 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Kpdotcom::Application.initialize!
-
-# Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
-
-Rails::Initializer.run do |config|
-
-  # Add additional load paths for your own custom dirs
-  config.autoload_paths += %W( #{RAILS_ROOT}/app/sweepers )
-
-  config.action_controller.session = {
-    :session_key => '_kpdotcom_session',
-    :secret      => '1be0c3c6354399c6274a2907772f4a0d8c08214e35057b18e42372098c439f4402b50bf477efe9073dd4749dcf747230d9535fc4ca5c7a1a697a0c1946d5ffe3'
-  }
-
-  # Activate observers that should always be running
-  config.active_record.observers = :contact_observer, :comment_observer
-
-  config.time_zone = 'Sydney'
-  
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = "test"
+ENV["Rails.env"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec'
 require 'spec/rails'
@@ -12,7 +12,7 @@ Spec::Runner.configure do |config|
   # in your config/boot.rb
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+  config.fixture_path = Rails.root + '/spec/fixtures/'
   config.include(CustomMatchers)
 
   # == Fixtures
@@ -29,10 +29,6 @@ Spec::Runner.configure do |config|
   #
   # If you declare global fixtures, be aware that they will be declared
   # for all of your examples, even those that don't use them.
-  #
-  # You can also declare which fixtures to use (for example fixtures for test/fixtures):
-  #
-  # config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
   #
   # == Mock Framework
   #
