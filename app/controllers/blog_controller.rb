d60daf8315ac1b092f8926211c51b4037ba926e1
Keith Pitty <keith@keithpitty.com>
2011-02-25 08:26:43 +1100

    Updated acts_as_taggable_on_steroids plugin together with other minor changes.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -4,6 +4,5 @@ class BlogController < ApplicationController
   def index
     @section = 'blog'
     @blog_posts = BlogPost.published.limit(5)
-    logger.debug "Latest post: #{@blog_posts.first.title}"
   end
 end
