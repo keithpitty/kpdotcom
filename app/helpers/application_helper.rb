c1c315f2018faa957246b9c559ed94caf0042687
Keith Pitty <keith@keithpitty.com>
2011-02-24 22:50:05 +1100

    Fixes to textile formatting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  include TagsHelper
+  include TagsHelper 
+  
+  def textilize(text)  
+    RedCloth.new(text).to_html.html_safe unless text.blank?  
+  end
   
   def coderay(text)
     text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do
