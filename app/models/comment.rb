939d9e19f899c6c63c894b9bfb1e2b5e8e4efbca
Keith Pitty <keith@keithpitty.com>
2011-04-28 02:24:40 +1000

    Touch blog post to force cache fragment of blog post to expire when comment has been saved.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   include Rakismet::Model
   
-  belongs_to :blog_post
+  belongs_to :blog_post, :touch => true
   validates_presence_of :name
   validates_presence_of :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, 
