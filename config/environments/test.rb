2cb9ac51031b9d0a20df0e57ff3a2b9fe6b4975f
Keith Pitty <keith@keithpitty.com>
2011-04-28 01:50:02 +1000

    Get rid of deprecation warnings.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,7 @@
 Kpdotcom::Application.configure do
   config.cache_classes = true
   config.whiny_nils = true
-  config.action_controller.consider_all_requests_local = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching             = false
   config.action_mailer.delivery_method = :test
 end
