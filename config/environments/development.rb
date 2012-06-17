64afa8796e930a59e15c0a07a238a2b65bab40b4
Keith Pitty <keith@keithpitty.com>
2012-06-17 12:21:05 +1000

    Upgrade to Rails 3.2.5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,23 @@ Kpdotcom::Application.configure do
   config.consider_all_requests_local = true
   config.action_controller.perform_caching             = false
   # config.action_view.cache_template_extensions         = false
-  config.action_view.debug_rjs                         = true
+  # config.action_view.debug_rjs                         = true
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
 end
