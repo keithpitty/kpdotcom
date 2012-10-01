9bee1c9bdaaabd63fe7372443e400a4f5039f44e
Keith Pitty <keith@keithpitty.com>
2012-10-01 15:48:29 +1000

    Add test for blog post param.

--- test/factories.rb
+++ test/factories.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do
+  factory :blog_post do
+    title { generate(:random_string) }
+    post { generate(:random_string) }
+  end
   factory :testimonial do
     provider_name { generate(:random_string) }
     provider_position { generate(:random_string) }
