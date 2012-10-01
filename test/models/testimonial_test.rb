22959458c1d766d3d90c62dd4b71ea20e019a9da
Keith Pitty <keith@keithpitty.com>
2012-10-01 14:13:33 +1000

    Add testimonial test.

---
+++ test/models/testimonial_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+describe Testimonial do
+  it "displays provider using name and position" do
+    testimonial = Testimonial.create!(provider_name: 'Fred Smith', provider_position: 'Director')
+    testimonial.provider.must_equal "#{testimonial.provider_name}, #{testimonial.provider_position}"
+  end
+end
