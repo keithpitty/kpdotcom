8f98c33936c8319c6666d7175238cf0b3f0ddaf0
Keith Pitty <keith@keithpitty.com>
2013-01-27 23:21:42 +1100

    Fix

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -3,7 +3,7 @@
 class Testimonial < ActiveRecord::Base
   default_scope order: 'rank ASC'
   validates_presence_of :rank
-  validates_numericality :rank
+  validates_numericality_of :rank
   validates_presence_of :provider_name
   validates_presence_of :provider_position
   validates_presence_of :recommendation
