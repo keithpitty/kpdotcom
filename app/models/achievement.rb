5fdb6067052e9eb75dba86b85700a387307212d6
Keith Pitty <keith@keithpitty.com>
2013-01-27 23:34:31 +1100

    Add validation to achievement model.

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,4 +2,9 @@
 
 class Achievement < ActiveRecord::Base
   default_scope order: 'rank ASC'
+  validates_presence_of :rank
+  validates_numericality_of :rank
+  validates_presence_of :heading
+  validates_presence_of :description
+  validates_presence_of :recommendation
 end
