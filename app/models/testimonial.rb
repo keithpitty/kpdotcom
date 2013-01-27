4af72c080b891730d5d77da6a6033d5dc8dfaa2c
Keith Pitty <keith@keithpitty.com>
2013-01-27 23:21:22 +1100

    Styled admin testimonial form and added validation.

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -2,6 +2,11 @@
 
 class Testimonial < ActiveRecord::Base
   default_scope order: 'rank ASC'
+  validates_presence_of :rank
+  validates_numericality :rank
+  validates_presence_of :provider_name
+  validates_presence_of :provider_position
+  validates_presence_of :recommendation
 
   def provider
     "#{provider_name}, #{provider_position}"
