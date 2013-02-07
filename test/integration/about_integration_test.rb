510937c1c5ce03a5914d1056ebee45173afc864b
Keith Pitty <keith@keithpitty.com>
2013-02-07 11:32:24 +1100

    Fix about integration test.

--- test/integration/about_integration_test.rb
+++ test/integration/about_integration_test.rb
@@ -4,8 +4,8 @@ describe "About integration" do
   it "shows testimonials and achievements" do
     achievement_1 = create(:achievement, rank: 1, heading: 'Achievement 1')
     achievement_2 = create(:achievement, rank: 2, heading: 'Achievement 2')
-    testimonial_1 = create(:testimonial, rank: 1, provider_name: 'Richie Benaud')
-    testimonial_2 = create(:testimonial, rank: 2, provider_name: 'Ian Chappell')
+    testimonial_1 = create(:testimonial, rank: 1, provider_name: 'Richie Benaud', recommendation: "Top bloke!")
+    testimonial_2 = create(:testimonial, rank: 2, provider_name: 'Ian Chappell', recommendation: "Bloody good bloke!")
     visit about_path
     page.text.must_include 'Achievement 1'
     page.text.must_include 'Achievement 2'
