e4843875dad97d758b47c3be4c647948d6018d86
Keith Pitty <keith@keithpitty.com>
2013-02-07 11:30:58 +1100

    Fixed admin testimonials integration test.

--- test/integration/admin_testimonials_integration_test.rb
+++ test/integration/admin_testimonials_integration_test.rb
@@ -3,8 +3,8 @@ require "minitest_helper"
 describe "Admin testimonials integration" do
   it "shows testimonials" do
     create(:user)
-    create(:testimonial, rank: 1, provider_name: "Richie")
-    create(:testimonial, rank: 2, provider_name: "Ian")
+    create(:testimonial, rank: 1, provider_name: "Richie", recommendation: "Top bloke!")
+    create(:testimonial, rank: 2, provider_name: "Ian", recommendation: "Bloody good bloke!")
     login
     visit admin_testimonials_path
     page.text.must_include "Richie"
