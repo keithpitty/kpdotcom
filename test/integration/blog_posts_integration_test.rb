c414c894f41c92f944ba8524159f66f057bf4e06
Keith Pitty <keith@keithpitty.com>
2013-02-07 14:54:08 +1100

    Add integration test for blog archives index.

--- test/integration/blog_posts_integration_test.rb
+++ test/integration/blog_posts_integration_test.rb
@@ -7,4 +7,21 @@ describe "Blog posts integration" do
     page.text.must_include 'Blog Post Title'
     page.text.must_include 'Some text'
   end
+
+  it "shows the archives page" do
+    create(:blog_post, title: 'Blog Post 1', post: 'Some text', published: true)
+    create(:blog_post, title: 'Blog Post 2', post: 'Some text', published: true)
+    create(:blog_post, title: 'Blog Post 3', post: 'Some text', published: true)
+    create(:blog_post, title: 'Blog Post 4', post: 'Some text', published: true)
+    create(:blog_post, title: 'Blog Post 5', post: 'Some text', published: true)
+    create(:blog_post, title: 'Blog Post 6', post: 'Some text', published: true)
+    visit blog_archives_path
+    page.text.must_include 'Archives'
+    page.text.must_include 'Blog Post 1'
+    page.text.must_include 'Blog Post 2'
+    page.text.must_include 'Blog Post 3'
+    page.text.must_include 'Blog Post 4'
+    page.text.must_include 'Blog Post 5'
+    page.text.must_include 'Blog Post 6'
+  end
 end
