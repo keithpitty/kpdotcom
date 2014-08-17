c9fc73b6fcbe826902ea3650c622e24e0f26226a
Keith Pitty <keith@keithpitty.com>
2014-08-17 21:12:50 +1000

    Add facility to close comments on a post.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110225030905) do
+ActiveRecord::Schema.define(:version => 20140817105320) do
 
   create_table "achievements", :force => true do |t|
     t.integer  "rank"
@@ -35,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20110225030905) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "param"
+    t.boolean  "comments_open"
   end
 
   create_table "comments", :force => true do |t|
