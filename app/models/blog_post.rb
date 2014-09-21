66bcc41126c6b997a0464b4635d5be9f9bb61bc0
Keith Pitty <keith@keithpitty.com>
2014-09-21 23:08:59 +1000

    Set published_at and param correctly.

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -7,8 +7,8 @@ class BlogPost < ActiveRecord::Base
   validates_presence_of :post
   acts_as_taggable
   scope :published, :conditions => {:published => true}, :order => "created_at desc"
-  before_create :set_param_from_now
-  before_update :set_param_from_created_at
+  before_save :set_published_at, if: :being_published?
+  before_save :set_param
 
   def self.find_published_tagged_with(tag)
     temp = self.tagged_with(tag)
@@ -16,16 +16,16 @@ class BlogPost < ActiveRecord::Base
     result = temp.sort {|x,y| y.created_at <=> x.created_at}
   end
 
-  def set_param_from_now
-    self.param = param_from_time(Time.zone.now)
-  end
-
-  def set_param_from_created_at
-    self.param = param_from_time(created_at)
+  def set_param
+    published? ? set_param_from_published_at : set_param_from_now
   end
 
   def to_param
-    param_from_time(created_at)
+    self.param || param_from_time(Time.zone.now)
+  end
+
+  def set_published_at
+    self.published_at = Time.zone.now 
   end
 
   def status
@@ -37,7 +37,19 @@ class BlogPost < ActiveRecord::Base
   end
 
   private
-    def param_from_time(time)
-      "#{time.to_date.to_formatted_s(:db)}-#{title.downcase.gsub(/\s|\./,"-").gsub(',','').sub(/\?$/,'')}"
-    end
+  def param_from_time(time)
+    "#{time.to_date.to_formatted_s(:db)}-#{title.downcase.gsub(/\s|\./,"-").gsub(',','').sub(/\?$/,'')}"
+  end
+
+  def being_published?
+    self.published? && self.published_at.nil?
+  end
+
+  def set_param_from_now
+    self.param = param_from_time(Time.zone.now)
+  end
+
+  def set_param_from_published_at
+    self.param = param_from_time(published_at)
+  end
 end
