a5ef20377ebdce8e0c29c7672eaca8f3f75f4a93
Keith Pitty <keith@keithpitty.com>
2014-12-26 18:36:28 +1100

    Test and clean up admin testimonials controller.

--- app/controllers/admin/testimonials_controller.rb
+++ app/controllers/admin/testimonials_controller.rb
@@ -1,47 +1,48 @@
-# coding: utf-8
+module Admin
+  # Public: Controller for administration of testimonials.
+  # Facilitates CRUD actions.
+  class TestimonialsController < AdminLayoutController
+    before_filter :require_user
+    cache_sweeper :testimonial_sweeper, only: [:create, :update, :destroy]
 
-class Admin::TestimonialsController < AdminLayoutController
-
-  before_filter :require_user
-  cache_sweeper :testimonial_sweeper, :only => [:create, :update, :destroy]
-
-  def index
-    @testimonials = Testimonial.all
-  end
-
-  def new
-    @testimonial = Testimonial.new
-  end
-
-  def create
-    @testimonial = Testimonial.new(params[:testimonial])
-    begin
-      @testimonial.save!
-      flash[:notice] = 'Testimonial was successfully created!'
-      redirect_to admin_testimonials_url
-    rescue
-      render 'new'
+    def index
+      @testimonials = Testimonial.all
     end
-  end
 
-  def edit
-    @testimonial = Testimonial.find(params[:id])
-  end
+    def new
+      @testimonial = Testimonial.new
+    end
 
-  def update
-    @testimonial = Testimonial.find(params[:id])
-    begin
+    def create
+      @testimonial = Testimonial.new(params[:testimonial])
+      if @testimonial.save
+        flash[:notice] = 'Testimonial created.'
+        redirect_to admin_testimonials_url
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @testimonial = Testimonial.find(params[:id])
+    end
+
+    def update
+      @testimonial = Testimonial.find(params[:id])
       @testimonial.update_attributes!(params[:testimonial])
-      flash[:notice] = 'Testimonial was succesfully updated!'
-      redirect_to admin_testimonials_url
-    rescue
-      render 'edit'
+      if @testimonial.save
+        flash[:notice] = 'Testimonial updated.'
+        redirect_to admin_testimonials_url
+      else
+        render 'edit'
+      end
     end
-  end
 
-  def destroy
-    @testimonial = Testimonial.find(params[:id])
-    @testimonial.destroy
-    redirect_to admin_testimonials_url
+    def destroy
+      @testimonial = Testimonial.find(params[:id])
+      flash[:notice] = 'Testimonial deleted.'
+      @testimonial.destroy
+      redirect_to admin_testimonials_url
+    end
   end
 end
---
+++ spec/controllers/admin/testimonials_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe Admin::TestimonialsController do
+  before { mock_user_session }
+
+  describe '#create' do
+    it 'creates a testimonial' do
+      post :create, testimonial: { rank: '10',
+                                   provider_name: 'Joe Bloggs',
+                                   provider_position: 'Developer',
+                                   recommendation: 'Knows his stuff.' }
+      expect(response).to redirect_to('/admin/testimonials')
+      expect(flash[:notice]).to eq('Testimonial created.')
+    end
+  end
+
+  context 'testimonial exists' do
+    let(:testimonial) do
+      instance_double('Testimonial', id: 1,
+                                     rank: 10,
+                                     provider_name: 'Joe Bloggs',
+                                     provider_position: 'Developer',
+                                     recommendation: 'Knows his stuff.')
+    end
+
+    before do
+      expect(Testimonial).to receive(:find).and_return(testimonial)
+    end
+
+    describe '#update' do
+      it 'updates a testimonial' do
+        expect(testimonial).to receive(:update_attributes!)
+        expect(testimonial).to receive(:save).and_return(true)
+        post :update, testimonial: { id: '1',
+                                     rank: '10',
+                                     provider_name: 'Joe Bloggs',
+                                     provider_position: 'Developer',
+                                     recommendation: 'Knows his stuff.' }
+        expect(response).to redirect_to('/admin/testimonials')
+        expect(flash[:notice]).to eq('Testimonial updated.')
+      end
+    end
+
+    describe '#destroy' do
+      it 'destroys a testimonial' do
+        expect(testimonial).to receive(:destroy).and_return(true)
+        post :destroy, id: 1
+        expect(response).to redirect_to('/admin/testimonials')
+        expect(flash[:notice]).to eq('Testimonial deleted.')
+      end
+    end
+  end
+end
