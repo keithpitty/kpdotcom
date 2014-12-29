8d4a7d5c445d1c0dd0f7b2dd879030af7f0102e1
Keith Pitty <keith@keithpitty.com>
2014-12-30 00:39:49 +1100

    Add feature spec for admin adding blog post.

---
+++ spec/features/admin_adds_blog_post_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'Admin adds blog post' do
+  background do
+    create(:user,
+           login: 'admin',
+           password: 'password',
+           password_confirmation: 'password') 
+  end
+
+  scenario 'they add and save a blog post' do
+    visit '/admin'
+    fill_in 'Login', with: 'admin'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_content("Login successful!")
+    click_link 'Blog posts'
+    click_link 'Add blog post'
+    fill_in 'Title', with: 'Test Post'
+    fill_in 'Post', with: 'Testing...'
+    click_button 'Save Blog Post'
+    expect(page).to have_content('Test Post')
+    expect(page).to have_content('Draft')
+  end
+end
