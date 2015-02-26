c11d4596bf8913bfa15ec4da04da3d3261598053
Keith Pitty <keith@keithpitty.com>
2015-02-26 22:57:04 +1100

    Move cache expiration from sweeper to controller.

--- app/controllers/admin/blog_posts_controller.rb
+++ app/controllers/admin/blog_posts_controller.rb
@@ -3,7 +3,6 @@ module Admin
   # Facilitates CRUD and preview actions.
   class BlogPostsController < AdminLayoutController
     before_filter :require_user
-    cache_sweeper :blog_sweeper, only: [:create, :update, :destroy]
 
     def index
       @blog_posts = BlogPost.draft + BlogPost.published
@@ -39,8 +38,10 @@ module Admin
     end
 
     def destroy
-      blog_post = BlogPost.find_by_param(params[:id])
-      blog_post.destroy
+      @blog_post = BlogPost.find_by_param(params[:id])
+      @blog_post.destroy
+      expire_fragment_caches
+      expire_fragment "comments_for_blog_post_#{@blog_post.id}"
       flash[:notice] = 'Blog post deleted.'
       redirect_to admin_blog_posts_path
     end
@@ -49,6 +50,7 @@ module Admin
 
     def handle_create
       if @blog_post.save
+        expire_fragment_caches
         flash[:notice] = 'Blog post created.'
         redirect_to admin_blog_posts_url
       else
@@ -61,11 +63,19 @@ module Admin
       @blog_post = BlogPost.find_by_param(params[:id])
       @blog_post.update_attributes(params[:blog_post])
       if @blog_post.save
+        expire_fragment_caches
         flash[:notice] = 'Blog post updated.'
         redirect_to admin_blog_posts_url
       else
         render :edit
       end
+    end
+
+    def expire_fragment_caches
+      expire_fragment :recent_posts
+      expire_fragment :tag_cloud
+      expire_fragment "blog_post_in_list_#{@blog_post.id}"
+      expire_fragment "blog_post_detail_#{@blog_post.id}"
     end
   end
 end
