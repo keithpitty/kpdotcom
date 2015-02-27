b685903e3170128cfbbbb6c6edd42d5f1d4b2a27
Keith Pitty <keith@keithpitty.com>
2015-02-27 21:10:17 +1100

    No sweepers left to load!

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ end
 
 module Kpdotcom
   class Application < Rails::Application
-    config.autoload_paths += %W( #{Rails.root}/app/sweepers )
     config.session_store = {
       :session_key => '_kpdotcom_session',
       :secret      => '1be0c3c6354399c6274a2907772f4a0d8c08214e35057b18e42372098c439f4402b50bf477efe9073dd4749dcf747230d9535fc4ca5c7a1a697a0c1946d5ffe3'
