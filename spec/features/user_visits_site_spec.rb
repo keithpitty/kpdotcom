8e0382f0f6cd144041d024de93b56931661690c9
Keith Pitty <keith@keithpitty.com>
2019-02-25 22:47:28 +1100

    Fix feature specs
    It would appear that the Capybara API has changed. Hence this commit
    changes the expectations to conform to the new API.

--- spec/features/user_visits_site_spec.rb
+++ spec/features/user_visits_site_spec.rb
@@ -3,25 +3,30 @@ require 'rails_helper'
 feature 'User visits site' do
   scenario 'they visit the home page' do
     visit '/'
-    expect(page).to have_css 'h1', 'Home'
+    expect(page).to have_css 'h1'
+    expect(page).to have_content 'Home'
   end
 
   scenario 'they visit the blog' do
     visit '/'
     click_link 'Blog'
-    expect(page).to have_css 'h2', 'Navigation'
-    expect(page).to have_css 'h2', 'Latest Blog Posts'
+    expect(page).to have_css 'h2'
+    expect(page).to have_content 'Navigation'
+    expect(page).to have_css 'h2'
+    expect(page).to have_content 'Latest Blog Posts'
   end
 
   scenario 'they visit the about page' do
     visit '/'
     click_link 'About'
-    expect(page).to have_css 'h1', 'About me'
+    expect(page).to have_css 'h1'
+    expect(page).to have_content 'About me'
   end
 
   scenario 'they visit the contact page' do
     visit '/'
     click_link 'Contact'
-    expect(page).to have_css 'h1', 'Contact me'
+    expect(page).to have_css 'h1'
+    expect(page).to have_content 'Contact me'
   end
 end
