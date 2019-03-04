86166dcc4e304d2cc4ae73836e28a9d623fe6c72
Keith Pitty <keith@keithpitty.com>
2019-03-04 23:54:21 +1100

    Fix bug
    A bug in the emojify helper was preventing the textilize helper method
    from converting textile links.

--- spec/helpers/emoji_helper.rb
+++ spec/helpers/emoji_helper.rb
@@ -5,6 +5,13 @@ describe EmojiHelper, type: :helper do
     it "translates :coffee:" do
       expected = "<img alt=\"coffee\" src=\"/images/emoji/unicode/2615.png\" style=\"vertical-align:middle\" width=\"20\" height=\"20\" />"
       actual = emojify(":coffee:")
+      expect(actual).to eq(expected)
+    end
+
+    it "preserves a textile link" do
+      expected = "\"GitHub\":http://github.com"
+      actual = emojify("\"GitHub\":http://github.com")
+      expect(actual).to eq(expected)
     end
   end
 end
