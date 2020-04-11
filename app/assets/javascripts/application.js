8c34b7e2f28fdc2427bc309ae4d7270b889fdf9b
Keith Pitty <keith@keithpitty.com>
2020-04-11 21:25:07 +1000

    Adapt styles to Tailwind
    Whilst the majority of this commit is implementing the Tailwind styles
    and customising them, there are many other small changes. Unfortunately
    I haven't made small commits along the way. Here are some of the changes
    included in this commit:
    
    - adapt navigation and presentation to not use secondary content
    - add a testimonials path, controller and view
    - fix a bug in finding the previous and next blog posts
    - improve the responsive design so that the UX is better on small
      devices
    - show the latest blog post in the blog section
    - introduce a separate archives nav option to list all blog posts
    - update home page to include photo, list latest three blog posts and
      remove out of date content.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,11 @@ $(document).ready(function() {
     window.location = window.location.href + "#postcomment";
   }
 });
+
+function closeAlert(event){
+  let element = event.target;
+  while (element.nodeName !== "BUTTON"){
+    element = element.parentNode;
+  }
+  element.parentNode.parentNode.removeChild(element.parentNode);
+}
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,19 +18,30 @@ module ApplicationHelper
 
   def navbar_item(label, link)
     if active_link?(label)
-      content_tag(:li, link_to(label, link), class: 'active')
+      link_to(label, link, class: 'text-white hover:text-white pr-4')
     else
-      content_tag(:li, link_to(label, link))
+      link_to(label, link, class: 'text-blue-300 hover:text-white pr-4')
     end
   end
 
   def active_link?(label)
-    controller_name == label.downcase || exceptional_active_link?(label)
+    active_public_link?(label) || active_admin_link?(label)
+  end
+
+  def active_public_link?(label)
+    (controller_name == label.downcase && action_name == 'index') || exceptional_active_link?(label)
+  end
+
+  def active_admin_link?(label)
+    (request.path.include? 'admin') && (controller_name == label.downcase || exceptional_active_admin_link?(label))
   end
 
   def exceptional_active_link?(label)
     (controller_name == 'contacts' && label == 'Contact') ||
-      (controller_name == 'archives' && label == 'Blog') ||
-      (controller_name == 'tags' && label == 'Blog')
+      (controller_name == 'archives' && action_name == 'show' && label == 'Blog')
+  end
+
+  def exceptional_active_admin_link?(label)
+    ((controller_name == 'admin_layout') && label == 'Home') || ((controller_name == 'blog_posts') && label == 'Blog posts')
   end
 end
