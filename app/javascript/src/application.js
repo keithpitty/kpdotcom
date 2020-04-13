0b96f6148402517f6ea4d46fdf984729b1de8a29
Keith Pitty <keith@keithpitty.com>
2020-04-13 16:21:21 +1000

    Restructure front-end assets for Webpack
    It's time to move all the front-end assets to where Webpacker expects
    them to be. I've also made some other minor changes as part of this
    commit. Still more to do to completely remove the asset pipeline.

--- app/javascript/src/application.js
+++ app/javascript/src/application.js
@@ -1,8 +1,3 @@
-//= require jquery
-//= require jquery_ujs
-//= require_self
-//= require_tree .
-
 $(document).ready(function() {
   if ($('#postcomment').length > 0 && $('.error').length > 0) {
     window.location = window.location.href + "#postcomment";
