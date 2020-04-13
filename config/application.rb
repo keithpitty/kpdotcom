94f7349b2d5b3ea8f6de7618b5099dd9631308e5
Keith Pitty <keith@keithpitty.com>
2020-04-13 16:36:45 +1000

    Prepare to remove assets folder
    I think this is all I need to do in preparation for removing the
    `app/assets` folder.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+
 
 if defined?(Bundler)
   Bundler.require *Rails.groups(:assets => %w(development test))
@@ -15,22 +23,13 @@ module Kpdotcom
       :secret      => '1be0c3c6354399c6274a2907772f4a0d8c08214e35057b18e42372098c439f4402b50bf477efe9073dd4749dcf747230d9535fc4ca5c7a1a697a0c1946d5ffe3'
     }
     config.time_zone = 'Sydney'
-    config.encoding = "utf-8"
+    config.encoding = 'utf-8'
     config.filter_parameters += [:password]
     config.rakismet.key = 'ac7c797a41f0'
     config.rakismet.url = 'http://keithpitty.com/'
     config.generators do |g|
       g.test_framework :rspec
     end
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
-
-    # For Heroku
-    config.assets.initialize_on_precompile = false
 
     # Skip locale validation
     config.i18n.enforce_available_locales = false
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -8,18 +8,6 @@ Kpdotcom::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_files = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-
-  # Precompile additional assets
-  config.assets.precompile += %w( application-ie.css )
-
   # Configure AWS variables for paperclip
   config.paperclip_defaults = {
     storage: :s3,
