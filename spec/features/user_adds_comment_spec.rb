de54b3e84f7d768a3827b96f0dd8f4f9bd64edef
Keith Pitty <keith@keithpitty.com>
2021-04-18 21:02:33 +1000

    Add test
    Adding this test is arguably overkill but it does increase test
    coverage.

--- spec/features/user_adds_comment_spec.rb
+++ spec/features/user_adds_comment_spec.rb
@@ -15,4 +15,14 @@ feature 'User visits blog post' do
 
     expect(page).to have_css('#comments p')
   end
+
+  scenario 'they try to add a comment without entering all necessary fields' do
+    visit '/'
+    click_link 'Blog'
+    fill_in 'Name', with: 'A Tester'
+    fill_in 'Comment', with: 'I agree!'
+    click_button 'Post Comment'
+
+    expect(page).to have_text('Please fix the errors and try again')
+  end
 end
