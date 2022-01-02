c0621758b85b91069677aa858b3b60605f1f355f
Keith Pitty <keith@keithpitty.com>
2022-01-02 21:56:47 +1100

    Update Rails to 7.0.0

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ end
 module Kpdotcom
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
     config.session_store = {
       :session_key => '_kpdotcom_session',
       :secret      => '1be0c3c6354399c6274a2907772f4a0d8c08214e35057b18e42372098c439f4402b50bf477efe9073dd4749dcf747230d9535fc4ca5c7a1a697a0c1946d5ffe3'
